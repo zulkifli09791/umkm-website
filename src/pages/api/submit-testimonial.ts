fc1baab6d57938a35a1eb14661ff2cd3e96815ac
zulkifli09791 <zulkiflikifli91219@gmail.com>
2025-09-16 13:31:56 +0700

    upp

--- src/pages/api/submit-testimonial.ts
+++ src/pages/api/submit-testimonial.ts
@@ -1,5 +1,5 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
-import { supabase } from '../../../lib/supabaseClient';
+import { supabase } from '../../lib/supabaseClient';
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   if (req.method !== 'POST') {
